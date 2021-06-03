f5e104d4bb69f262023ad4a834355bf81be11e8c
aleem1314 <aleem@vitwit.com>
2021-06-03 16:20:13 +0530

    test

--- server/grpc/grpc_web.go
+++ server/grpc/grpc_web.go
@@ -11,13 +11,22 @@ import (
 
 // StartGRPCWeb starts a gRPC-Web server on the given address.
 func StartGRPCWeb(grpcSrv *grpc.Server, config config.Config) (*http.Server, error) {
+
 	wrappedServer := grpcweb.WrapServer(grpcSrv)
-	handler := func(resp http.ResponseWriter, req *http.Request) {
-		wrappedServer.ServeHTTP(resp, req)
-	}
+	handler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, x-user-agent, x-grpc-web, grpc-status, grpc-message")
+		w.Header().Set("Access-Control-Expose-Headers", "grpc-status, grpc-message")
+		if req.Method == "OPTIONS" {
+			return
+		}
+
+		wrappedServer.ServeHTTP(w, req)
+	})
+
 	grpcWebSrv := &http.Server{
 		Addr:    config.GRPCWeb.Address,
-		Handler: http.HandlerFunc(handler),
+		Handler: handler,
 	}
 	if err := grpcWebSrv.ListenAndServe(); err != nil {
 		return nil, err
